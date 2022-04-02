0fcc9af1b327cd19eaca267ed980694f2294040a
fuka-innovations <fuad.kalhori@gmail.com>
2022-04-02 10:01:11 +0300

    Device mediaPlayer volume is set to MAX value on app start

--- VolumeDictator/ContentView.swift
+++ VolumeDictator/ContentView.swift
@@ -6,13 +6,49 @@
 //
 
 import SwiftUI
+import MediaPlayer
+import WebKit
 
 struct ContentView: View {
+    @State private var soundLevel: Float = 0.0
+    @State private var showWebView = false
+    
     var body: some View {
         Text("I'am the only volume dictator, and that is it!")
             .padding()
+        
+        Section {
+            Button {
+                showWebView.toggle()
+            } label: {
+                Text("Play youtube video")
+            }
+            .sheet(isPresented: $showWebView) {
+                WebView(url: URL(string: "https://www.youtube.com/watch?v=P3ALwKeSEYs")!)
+            }
+        }
+        
+        
     }
 }
+
+
+
+
+struct WebView: UIViewRepresentable {
+ 
+    var url: URL
+ 
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+ 
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- VolumeDictator/VolumeDictatorApp.swift
+++ VolumeDictator/VolumeDictatorApp.swift
@@ -6,12 +6,32 @@
 //
 
 import SwiftUI
+import MediaPlayer
+
 
 @main
 struct VolumeDictatorApp: App {
+    init() {
+        MPVolumeView.setVolume(1.0)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+    
+}
+
+
+
+extension MPVolumeView {
+    static func setVolume(_ volume: Float) -> Void {
+        let volumeView = MPVolumeView()
+        let slider = volumeView.subviews.first(where: { $0 is UISlider }) as? UISlider
+
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.01) {
+            slider?.value = volume
         }
     }
 }
