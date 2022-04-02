019ca40ba231494ac14d0600bd532bb2d2f3a613
fuka-innovations <fuad.kalhori@gmail.com>
2022-04-02 08:57:56 +0300

    Initial Commit

---
+++ VolumeDictator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VolumeDictator
+//
+//  Created by FUKA on 2.4.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("I'am the only volume dictator, and that is it!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VolumeDictator/VolumeDictatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeDictatorApp.swift
+//  VolumeDictator
+//
+//  Created by FUKA on 2.4.2022.
+//
+
+import SwiftUI
+
+@main
+struct VolumeDictatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
